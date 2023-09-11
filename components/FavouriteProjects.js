d91f6d61029cdd2240f951eaa4e98baa2bbba7b6
TeodoroTopa <Topaman2@gmail.com>
2023-09-10 19:46:49 -0500

    readjust project title alignment and apply to hero page

--- components/FavouriteProjects.js
+++ components/FavouriteProjects.js
@@ -47,7 +47,7 @@ export default function FavouriteProjects() {
                 alt="portfolio"
                 className="transform hover:scale-125 transition duration-2000 ease-out"
               />
-              <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
+              <h1 className="absolute top-10 left-10 right-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
               Loisaba EarthRanger & Tableau Mapping Project
               </h1>
               <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
@@ -68,7 +68,7 @@ export default function FavouriteProjects() {
                 alt="portfolio"
                 className="transform hover:scale-125 transition duration-2000 ease-out"
               />
-              <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
+              <h1 className="absolute top-10 left-10 right-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
               Cornell Course Recommendation Website
               </h1>
               <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
@@ -88,7 +88,7 @@ export default function FavouriteProjects() {
                 alt="portfolio"
                 className="transform hover:scale-125 transition duration-2000 ease-out object-cover shadow-2xl"
               />
-              <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
+              <h1 className="absolute top-10 left-10 right-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
                 Sample Projects
               </h1>
               <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
