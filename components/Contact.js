11e16fed1884f506f1fcf0913d57215b8cdbb616
Teodoro Topa <teodorotopa@gmail.com>
2024-03-25 15:49:11 -0500

    Update google maps href.js

--- components/Contact.js
+++ components/Contact.js
@@ -102,7 +102,7 @@ export default function Contact() {
                 >
                   <path d="M4.146.146A.5.5 0 0 1 4.5 0h7a.5.5 0 0 1 .5.5c0 .68-.342 1.174-.646 1.479-.126.125-.25.224-.354.298v4.431l.078.048c.203.127.476.314.751.555C12.36 7.775 13 8.527 13 9.5a.5.5 0 0 1-.5.5h-4v4.5c0 .276-.224 1.5-.5 1.5s-.5-1.224-.5-1.5V10h-4a.5.5 0 0 1-.5-.5c0-.973.64-1.725 1.17-2.189A5.921 5.921 0 0 1 5 6.708V2.277a2.77 2.77 0 0 1-.354-.298C4.342 1.674 4 1.179 4 .5a.5.5 0 0 1 .146-.354z" />
                 </svg>
-                <a target="_blank" href="https://www.google.com/maps/place/Chicago,+IL,+USA/@41.8337734,-88.061626,10z/data=!4m6!3m5!1s0x880e2c3cd0f4cbed:0xafe0a6ad09c0c000!8m2!3d41.8781136!4d-87.6297982!16zL20vMDFfZDQ?entry=ttu" className="text-gray-50 font-light text-sm">
+                <a target="_blank" href="https://www.google.com/maps/place/New+York,+NY/@40.6970193,-74.3093371,10z/data=!3m1!4b1!4m6!3m5!1s0x89c24fa5d33f083b:0xc80b8f06e177fe62!8m2!3d40.7127753!4d-74.0059728!16zL20vMDJfMjg2?entry=ttu" className="text-gray-50 font-light text-sm">
                   {userData.address}
                 </a>
               </div>
